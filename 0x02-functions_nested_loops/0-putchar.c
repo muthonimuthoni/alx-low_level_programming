91aff4153e3c68031d8a92d5ed20e707974cccc7
muthonimuthoni <shanicemuthoni442@gmail.com>
2023-10-11 14:58:07 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - entry point of the program
+ *
+ * Description: program that prints "_putchar" followed by a new line
+ *
+ * Return: 0
+ */
+
+int main (void)
+{
+	printf("_putchar\n");
+	return(0);
+}
+
+
