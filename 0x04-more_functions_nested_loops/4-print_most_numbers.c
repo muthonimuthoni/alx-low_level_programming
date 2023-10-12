f100e6f9e0a502d29b54bdef1a2b153db83335d1
SHANICE MUTHONI <63288182+muthonimuthoni@users.noreply.github.com>
2023-10-13 02:32:01 +0300

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,16 +5,16 @@
  */
 void print_most_numbers(void)
 {
-    char number;
+	char number;
 
-    for (number = '0'; number <= '9'; number++)
-    {
-        if (number != '2' && number != '4')
-        {
-            _putchar(number);
-        }
-    }
-    _putchar('\n');
+	for (number = '0'; number <= '9'; number++)
+	{
+		if (number != '2' && number != '4')
+		{
+			_putchar(number);
+		}
+	}
+	_putchar('\n');
 }
 
 
